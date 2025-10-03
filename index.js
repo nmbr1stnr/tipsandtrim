2f7079633063d641f8bbaa07ac284ca746e9b91a
nmbr1stnr <162379848+nmbr1stnr@users.noreply.github.com>
2025-10-02 19:12:00 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY_1);
 const app = express();
 const PORT = process.env.PORT || 3000;
 
